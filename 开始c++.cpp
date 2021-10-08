837700e86837bdbfa555f53c9649ad8e086f90c3
BENBoneday <85631569+BENBoneday@users.noreply.github.com>
2021-10-08 21:41:03 +0800

    Add files via upload

---
+++ 开始c++.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	int a = 10;
+	cin >> a;//¼üÅÌÊäÈë
+	string str = "sadasd";
+	cout << str << endl;//Êä³ö
+	cout << "hello world a="<<a<< endl;
+	system("pause");
+	return 0;
+}
